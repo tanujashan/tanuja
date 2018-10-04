03560ec9fecc671d0f6fcc55081e1fa7ea09236e
tanujashan <43032009+tanujashan@users.noreply.github.com>
2018-10-04 16:16:43 +0530

    Add files via upload

---
+++ tables2.py
@@ -0,0 +1,18 @@
+n=int(input("enter a value"))
+for i in range(n,n+1):
+	for j in range(1,11):
+		print(i,"X",j,"=",i*j)
+
+output:
+  enter a value5
+	5 X 1 = 5
+	5 X 2 = 10
+	5 X 3 = 15
+	5 X 4 = 20
+	5 X 5 = 25
+	5 X 6 = 30
+	5 X 7 = 35
+	5 X 8 = 40
+	5 X 9 = 45
+	5 X 10 = 50
+
